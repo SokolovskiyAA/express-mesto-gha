fd93c7442738f99234905f2814e3f0901b9ce5f7
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-07-19 21:19:33 +0300

    fix

--- models/users.js
+++ models/users.js
@@ -6,18 +6,15 @@ const userSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 30,
     required: true,
-    default: 'Соколовский Андрей',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
     required: true,
-    default: 'Разработчик',
   },
   avatar: {
     type: String,
-    default: 'https://bipbap.ru/wp-content/uploads/2018/09/a6959bb1263fe6ad530534c2bb942bd8.jpg',
     required: true,
   },
 });
