7e8c6e2818ab8942a0c8006b8b72399579dea7f7
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-07-20 20:06:50 +0300

    fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -14,7 +14,7 @@ module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      res.status(status.CREATED).res.send(card);
+      res.status(status.CREATED).send(card);
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
