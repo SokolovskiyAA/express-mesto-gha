337eee2e4efd268fc369cabfbc45b2468ccdb774
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-08-18 23:11:12 +0300

    fix

--- app.js
+++ app.js
@@ -21,37 +21,37 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 // Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'https://mesto.sokolovskiy.students.nomoredomains.sbs',
-  'http://mesto.sokolovskiy.students.nomoredomains.sbs',
-  'localhost:3000'
-];
+// const allowedCors = [
+//   'https://mesto.sokolovskiy.students.nomoredomains.sbs',
+//   'http://mesto.sokolovskiy.students.nomoredomains.sbs',
+//   'localhost:3000'
+// ];
 
-app.use(function (req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+// app.use(function (req, res, next) {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
 
-    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//     const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
-    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-    // сохраняем список заголовков исходного запроса
-    const requestHeaders = req.headers['access-control-request-headers'];
-    // Если это предварительный запрос, добавляем нужные заголовки
-    if (method === 'OPTIONS') {
-      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      // разрешаем кросс-доменные запросы с этими заголовками
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      // завершаем обработку запроса и возвращаем результат клиенту
-      return res.end();
-    }
-  }
+//     // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//     const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+//     // сохраняем список заголовков исходного запроса
+//     const requestHeaders = req.headers['access-control-request-headers'];
+//     // Если это предварительный запрос, добавляем нужные заголовки
+//     if (method === 'OPTIONS') {
+//       // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//       // разрешаем кросс-доменные запросы с этими заголовками
+//       res.header('Access-Control-Allow-Headers', requestHeaders);
+//       // завершаем обработку запроса и возвращаем результат клиенту
+//       return res.end();
+//     }
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(requestLogger);
 
