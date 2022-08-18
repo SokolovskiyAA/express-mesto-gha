dddb899cff14f4aa320e41ebbe42c06ec23fa5c5
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-08-18 22:56:49 +0300

    qwe

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
 const NotFoundError = require('./errors/not-found-error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { usersRouter } = require('./routes/users');
 const { cardsRouter } = require('./routes/cards');
@@ -18,6 +19,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+// Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://http://mesto.sokolovskiy.students.nomoredomains.sbs',
+  'http://http://mesto.sokolovskiy.students.nomoredomains.sbs',
+  'localhost:3000'
+];
+
+app.use(function (req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+
+    const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+    // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+    // сохраняем список заголовков исходного запроса
+    const requestHeaders = req.headers['access-control-request-headers'];
+    // Если это предварительный запрос, добавляем нужные заголовки
+    if (method === 'OPTIONS') {
+      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      // разрешаем кросс-доменные запросы с этими заголовками
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      // завершаем обработку запроса и возвращаем результат клиенту
+      return res.end();
+    }
+  }
+
+  next();
+});
+
+app.use(requestLogger);
 
 app.use('/signin', celebrate({
   body: Joi.object().keys({
@@ -43,6 +79,8 @@ app.use('/cards', cardsRouter);
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 
