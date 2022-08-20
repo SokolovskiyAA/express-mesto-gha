0e9928a14f837a562350cf56442301ca0800f7b0
SokolovskiyAA <sokolovsky.a.a@yandex.ru>
2022-08-20 11:27:31 +0300

    fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { usersRouter } = require('./routes/users');
 const { cardsRouter } = require('./routes/cards');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
